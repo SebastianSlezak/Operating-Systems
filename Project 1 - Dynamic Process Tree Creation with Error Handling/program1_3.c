c963d372203997a7610199d2fda7cc1a32288da1
Sebastian <szalassje@gmail.com>
2024-01-03 19:35:41 +0100

    Implementation of communication with child processes using the exec() function
    This commit modifies an existing program (program1_2.c) to use the exec() function to run a separate program for each child process. The master process now waits for all slave processes to finish, draws the process tree and prints the completion status of each slave process.
    
    Changes made:
    - The child processes now execute a separate program using the exec() function.
    - The master process waits for slave processes and displays the process tree.
    - Error handling using perror() for fork(), exec() and wait().
    
    This update improves code clarity and separates the responsibilities of master and slave processes, providing a better understanding of program behavior.

---
+++ Project 1 - Dynamic Process Tree Creation with Error Handling/program1_3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/errno.h>
+
+int main() {
+
+    char cmd[20];
+    int w, x;
+
+    sprintf(cmd, "pstree -p %d", getpid());
+    system(cmd);
+
+    for (int i = 0; i < 3; i++) {
+        switch (fork()) {
+            case -1:
+                perror("fork error");
+                exit(1);
+                break;
+            case 0:
+                /*Proces potomny*/
+                if (execl("./program1_1", "program1_1", NULL) == -1) {
+                    perror("execl error");
+                }
+                exit(2);
+                break;
+            default:
+                /*Proces macierzysty*/
+                printf("Action for the process parent.\n");
+                break;
+        }
+    }
+
+    system(cmd);
+
+    for (int i = 0; i < 3; ++i) {
+        w = wait(&x);
+
+        if (w == -1) {
+            perror("wait error");
+            exit(3);
+        }
+        printf("The process %d ended with: %d \n", w, x);
+    }
+
+    return 0;
+}
