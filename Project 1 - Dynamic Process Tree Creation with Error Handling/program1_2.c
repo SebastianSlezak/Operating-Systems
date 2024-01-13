ee21ab163a789f9f8078ac93b280684a29eebf8e
Sebastian <szalassje@gmail.com>
2024-01-13 22:30:31 +0100

    Fixed pstree in program1_2

--- Project 1 - Dynamic Process Tree Creation with Error Handling/program1_2.c
+++ Project 1 - Dynamic Process Tree Creation with Error Handling/program1_2.c
@@ -3,32 +3,39 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main() {
-
+int main()
+{
+    int pid = getpid();
     char cmd[20];
 
-    sprintf(cmd, "pstree -p %d", getpid());
+    sprintf(cmd, "pstree -p %d", pid);
     system(cmd);
 
-    for (int i = 0; i < 3; i++) {
-        switch (fork()) {
-            case -1:
-                perror("fork error");
-                exit(1);
-                break;
-            case 0:
-                printf("PID: %d\n", getpid());
-                printf("PPID: %d\n", getppid());
-                printf("UID: %d\n", getuid());
-                printf("GID: %d\n", getgid());
-                break;
-            default:
-                printf("Action for the process parent.\n");
-                break;
+    for (int i = 0; i < 3; i++)
+    {
+        switch (fork())
+        {
+        case -1:
+            perror("fork error");
+            exit(1);
+            break;
+        case 0:
+            printf("PID: %d\n", getpid());
+            printf("PPID: %d\n", getppid());
+            printf("UID: %d\n", getuid());
+            printf("GID: %d\n", getgid());
+            break;
+        default:
+            printf("Action for the process parent.\n");
+            break;
         }
     }
 
-    system(cmd);
+    if (getpid() == pid)
+    {
+        system(cmd);
+    }
 
+    sleep(1);
     return 0;
 }
